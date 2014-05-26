6207a008158838e029d22afe89617881f99f88cc
zihpzhong <shengshi.zhong@gmail.com>
2014-05-26 16:45:24 +0800

    Change-Id: I6f198d9040e19b173d87babe2ff6f30025a999d5 Signed-off-by: zihpzhong <shengshi.zhong@gmail.com>

---
+++ tradecoin/src/main/java/com/ziphchung/tradecoin/tool/EchoServerThread.java
@@ -0,0 +1,13 @@
+package com.ziphchung.tradecoin.tool;
+
+public class EchoServerThread implements Runnable {
+	public EchoServerThread () {
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("----------------------------------");
+	}
+	
+}
